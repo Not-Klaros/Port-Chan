5e2d552fdc59640c91210888e41511f9a07d7893
Carlos Rodriguez <71926615+Not-Klaros@users.noreply.github.com>
2025-06-26 12:47:52 +1000

    style: add blog layout to feed

--- my-portfolio/src/pages/Feed.tsx
+++ my-portfolio/src/pages/Feed.tsx
@@ -1,2 +1,38 @@
-const Feed = () => <div><h1>Feed</h1><p>This is your feed. Add cool updates here.</p></div>;
+const posts = [
+  {
+    title: 'First Update',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.',
+  },
+  {
+    title: 'Another Update',
+    body:
+      'Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+  },
+  {
+    title: 'Final Update',
+    body:
+      'Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.',
+  },
+];
+
+const Feed = () => (
+  <div className="feed-layout">
+    <div className="feed-left">
+      <h1>Feed</h1>
+      <p className="profile-description">
+        Hey there! I&#39;m Carlos, a developer who loves building web experiences.
+      </p>
+    </div>
+    <div className="feed-right">
+      {posts.map((post) => (
+        <div key={post.title} className="feed-block">
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
 export default Feed;
