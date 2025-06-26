4b28de459e26be79236784af6407d453d3076e55
Carlos Rodriguez <71926615+Not-Klaros@users.noreply.github.com>
2025-06-26 19:06:50 +1000

    Add images for projects and update Projects page

--- my-portfolio/src/pages/Projects.tsx
+++ my-portfolio/src/pages/Projects.tsx
@@ -1,25 +1,41 @@
-const projectTitles = ['Project One', 'Project Two', 'Project Three'];
+import Project1Image from '../assets/projects/project1.svg';
+import Project2Image from '../assets/projects/project2.svg';
+import Project3Image from '../assets/projects/project3.svg';
+
+interface Project {
+  title: string;
+  image: string;
+}
+
+const projects: Project[] = [
+  { title: 'Project One', image: Project1Image },
+  { title: 'Project Two', image: Project2Image },
+  { title: 'Project Three', image: Project3Image },
+];
 
 const Projects = () => (
   <div className="projects-layout">
     <div className="project-left">
       <h1 className="sticky-title">Projects</h1>
-      {projectTitles.map((title) => (
-        <div key={title} className="category-box">
-          {title}
+      {projects.map((project) => (
+        <div key={project.title} className="category-box">
+          {project.title}
         </div>
       ))}
     </div>
     <div className="project-right">
       <div className="project-grid">
-        {projectTitles.map((title) => (
-          <div key={title} className="project-item">
-            <h2>{title}</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        {projects.map((project) => (
+          <div key={project.title} className="project-item">
+            <h2>{project.title}</h2>
+            <img src={project.image} alt={project.title} />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
               Magnam numquam, autem facere officia placeat eum unde quas aspernatur
               eligendi deleniti corporis, libero cupiditate id esse eaque at iure nemo eos.
               Maiores inventore laborum expedita doloribus explicabo. Amet ut totam officiis
-              facilis maxime, est iure laborum.</p>
+              facilis maxime, est iure laborum.
+            </p>
           </div>
         ))}
       </div>
